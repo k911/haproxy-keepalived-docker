d65d0450826f72073deaa50a7628b5c40369aea7
k911 <konradobal@gmail.com>
2019-07-08 19:35:40 +0200

    build(ssh): Create ubuntu setup script

---
+++ ubuntu-setup.sh
@@ -0,0 +1,14 @@
+sudo su -
+
+apt update
+apt -y install docker.io dnsutils tshark ipvsadm
+docker --version
+
+# install docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
+
+# enable ip_vs
+modprobe ip_vs
